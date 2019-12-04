6a265ec5168a3262d020df1bc9d74c306cbaaad0
Brad <brad@viziworx.com>
2019-12-04 18:06:54 -0500

    setup endpoints

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const postRouter = require('./posts/postRouter.js')
 const server = express();
 
 // middleware
-
 server.use(helmet());
 server.use(express.json());
 server.use(logger)
@@ -17,11 +16,14 @@ server.get('/', (req, res) => {
 });
 
 //custom middleware
-
 function logger(req, res, next) {
-  console.log(`${req.method} to ${req.originalUrl}`);
+  console.log(`${req.method} to ${req.originalUrl} at ${Date.now()}`);
 
   next();
 }
+
+// endpoints
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter)
 
 module.exports = server;
