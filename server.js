86ef13469b3cdc14b5efab200439d4f3132d5c76
Brad <brad@viziworx.com>
2019-12-04 17:43:36 -0500

    Added middleware for helmet and express

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
+
+const userRouter = require('./users/userRouter.js')
+const postRouter = require('./posts/postRouter.js')
 
 const server = express();
+
+// middleware
+
+server.use(helmet());
+server.use(express.json());
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +18,8 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} to `)
+}
 
 module.exports = server;
