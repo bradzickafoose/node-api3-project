1c2f43dff332c90b8c37fd1d75406d7df082336d
Brad <brad@viziworx.com>
2020-05-14 12:13:20 -0400

    adds endpoint to get all posts and middleware to validate post id

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const Posts = require('./postDb');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Posts
+    .get()
+    .then(posts => res.status(200).json(posts))
+    .catch(() => res.status(500).json({
+      message: "Error retrieving posts from the database"
+    }))
 });
 
 router.get('/:id', (req, res) => {
@@ -21,7 +27,24 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const { id } = req.body;
+
+  Posts
+    .getById(id)
+    .then(post => {
+      if (post) {
+        req.post = post;
+        return next()
+      }
+      res.status(400).json({
+        message: "Invalid post ID"
+      });
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "Error retrieving the post"
+      });
+    });
 }
 
 module.exports = router;
